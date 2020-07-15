e338cbaa72de88bd498d9c1aacdf8bc3130e9ad3
S M Jahangir Alam <33924742+jahangir2526@users.noreply.github.com>
2020-07-15 15:25:06 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,6 +7,6 @@ export const config = {
     path_createMessage: 'posts/create',
     path_validate_otp: 'users/validateOTP',
     path_generate_otp: 'users/generateOTP',
-    obj_store_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/p/ShyUyJr14FZAa4rMfwNRvAFgPYzhLWp4IULM_vTv-yQ/n/gse00014894/b/sc_symposium/o/",
-    obj_store_read_url: "https://objectstorage.us-ashburn-1.oraclecloud.com/n/gse00014894/b/sc_symposium/o/",
+    obj_store_url: "https://objectstorage.ap-sydney-1.oraclecloud.com/p/RQS2-oX0uDIKPxizu53BQPn9N45AUEBUWmCSV37nteg/n/idx9wbxtnehn/b/bucket-symposium1/o/",
+    obj_store_read_url: "https://objectstorage.ap-sydney-1.oraclecloud.com/n/idx9wbxtnehn/b/bucket-symposium1/o/",
 }
